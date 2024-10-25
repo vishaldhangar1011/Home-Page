feaf09420ac75f110acab5fc4d273215c18e161f
Vishal Dhangar <vishaldhangar932@gmail.comcleargit config --global user.name Vishal>
2024-10-26 01:43:55 +0530

    adding some configure settings

--- script.js
+++ script.js
@@ -3,9 +3,20 @@ const nav = document.getElementById("navbar-multi-level");
 const menuIcon = document.getElementById("menu-icon");
 const closeIcon = document.getElementById("close-icon");
 
+//button clicked logic
 button.addEventListener("click", () => {
   nav.classList.toggle("hidden");
   nav.classList.toggle("block");
   menuIcon.classList.toggle("hidden");
   closeIcon.classList.toggle("hidden");
 });
+
+//checkmark logic
+function toggleTerms(checkbox) {
+  const checkmark = checkbox.nextElementSibling.querySelector('.checkmark');
+  if (checkbox.checked) {
+      checkmark.classList.remove('hidden');
+  } else {
+      checkmark.classList.add('hidden');
+  }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ['./index.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        playfair: ['Playfair Display', 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+      colors: {
+        customLightGray: '#E8E9EE', //
+        customGray: '#1B1B1E',
+        customGreen: '#02C4A6', //for button border
+        customBlue: '#0197DA',
+        customGradient: 'linear-gradient(90deg, #0096DB 0%, #02C4A5 100%)' //for buttons, input gradient
+        
+      },
+      
+    },
   },
   plugins: [],
 }
